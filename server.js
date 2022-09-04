25aecd5cba88934da4fbbd8f145fa70644f4490e
Mauricio Matus F <mauricio@Mauricios-MacBook-Pro.local>
2022-09-04 12:00:26 -0400

    commit01: comienzo del desarrollo del backend de mi protafolio. Sera con NodeJS y Express, Render desde Servidor con motor de tempaltes EJS.

---
+++ server.js
@@ -0,0 +1,30 @@
+//Importamos todas las funcionalidades de app. Capa logica negocios
+const app = require('../backend-portaf02/app');
+const port = 3000;
+
+
+//Aquí desarrollaremos capa controlador
+//EJS
+app.get('/', (req, res) => {
+  res.send("Comienzo desarrollo backend portafolio")
+});
+
+
+//Aquí desarrollaremos las peticiones de subscripción y contacto de cleinte
+//Base de datos simple, un CRUD
+app.post('/clientes', (req, res) => {
+  res.send('Recibi petición POST')
+});
+
+
+//Por mientras desarrollaremos aqui la opcion de borrar post
+//pero es mejor crear un dashboard de administración y separar este modulo
+app.delete('/user', (req, res) => {
+  res.send('Petición DELETE a ruta /usuario')
+});
+
+//Ejecutamos el puerto de escucha
+app.listen(port, ()=> {
+  console.log(`Ejecutando server en http://localhost:${port}`)
+});
+
